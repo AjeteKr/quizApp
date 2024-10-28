59ff5df5c869fc296376eac684b495ec38bd83c2
Ajete Krasniqi <ajetekr@gmail.com>
2024-10-28 19:43:47 +0100

    Setting Different Timers Based On The Selected Answer

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -9,6 +9,17 @@ export default function Question({index, onSelectAnswer, onSkipAnswer}) {
         selectedAnswer: '',
         isCorrect: null,
     });
+
+    let timer = 10000;
+
+    if (answer.selectedAnswer) {
+        timer = 1000;
+    }
+
+    if (answer.isCorrect !== null) {
+        timer = 2000;
+    }
+
     function handleSelectAnswer(answer) {
         setAnswer({
             selectedAnswer: answer,
@@ -35,7 +46,12 @@ export default function Question({index, onSelectAnswer, onSkipAnswer}) {
 
     return (
         <div id="question"> 
-            <QuestionTimer timeout={10000} onTimeout={onSkipAnswer}/>
+            <QuestionTimer 
+                key={timer} 
+                timeout={timer} 
+                onTimeout={answer.selectedAnswer === '' ? onSkipAnswer : null} 
+                mode={answerState}
+            />
             <h2>{QUSTIONS[index].text}</h2>
             <Answers 
                 answers={QUSTIONS[index].answers}
--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 
-export default function QuestionTimer({ timeout, onTimeout }) {
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
     const [remaingTime, setRemaingTime] = useState(timeout);
     
     useEffect(() => {
@@ -21,5 +21,5 @@ export default function QuestionTimer({ timeout, onTimeout }) {
         };
     }, []);
 
-    return <progress id="question-time" max={timeout} value={remaingTime}/>
+    return <progress id="question-time" max={timeout} value={remaingTime} className={mode}/>
 }
